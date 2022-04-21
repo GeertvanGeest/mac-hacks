2d44ac00c8f73d2389e01e62154aaa189e13a2b4
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2022-04-21 15:31:37 +0200

    removes imagemagick step

--- mov_to_gif.sh
+++ mov_to_gif.sh
@@ -5,8 +5,8 @@ MOV=$1
 BASENAME=${MOV%.mov}
 
 # -s 640x480
-ffmpeg  -i $MOV -pix_fmt rgb24 -r 30 tmp.gif
+ffmpeg  -i $MOV -pix_fmt rgb24 -r 10 $BASENAME.gif
 
 # optimizing
 # needs imagemagick
-convert -layers Optimize tmp.gif $BASENAME.gif
+# convert -layers Optimize tmp.gif $BASENAME.gif
